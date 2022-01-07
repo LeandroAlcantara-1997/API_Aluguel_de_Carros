5fe2578dafbbe7ae87ff514053af490c1eb104c7
Leandro Alcantara <leandro1997silva97@gmail.com>
2022-01-07 18:20:20 -0300

    br-15 Limpando o arquivo main

---
+++ aluguel/controller/router/routes.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	"net/http"
+
+	rest "github.com/LeandroAlcantara-1997/controller/rest"
+	"github.com/gorilla/mux"
+)
+
+func NewRouter() *mux.Router {
+	r := mux.NewRouter()
+	fs := http.FileServer(http.Dir("./view/assets"))
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fs))
+	//Cliente
+	r.HandleFunc("/cliente/cadastro", rest.GetCadastraCliente).Methods("GET") //Template
+	r.HandleFunc("/cliente/cadastro", rest.PostCadastraCliente).Methods("POST")
+	r.HandleFunc("/cliente/deleta", rest.DeletaCadastro).Methods("DELETE")
+	r.HandleFunc("/", rest.GetLoginCliente).Methods("GET") //Template
+	r.HandleFunc("/", rest.PostLoginCliente).Methods("POST")
+
+	r.HandleFunc("/recuperarSenha", rest.GetRestauraSenha).Methods("GET") //Template
+	r.HandleFunc("/recuperarSenha", rest.PostRestauraSenha).Methods("POST")
+
+	r.HandleFunc("/homeCliente", rest.HomeCliente) //Template
+
+	//Carros
+	r.HandleFunc("/carrosCadastrados", rest.GetCarrosCadastrados)
+	r.HandleFunc("/cadastraCarro", rest.CadastraCarro)
+	//r.HandleFunc("/carrosDisponiveis", rest.CarrosDisponiveis)
+
+	//Admin
+	r.HandleFunc("admin/loginAdmin", rest.GetLoginAdmin).Methods("GET") //Template
+	r.HandleFunc("admin/loginAdmin", rest.PostLoginAdmin).Methods("POST")
+	r.HandleFunc("admin/home", rest.HomeAdmin)
+	r.HandleFunc("/getIdCliente", rest.GetByIdCliente)
+	r.HandleFunc("/getClientes", rest.GetClientesCadastrados)
+
+	
+
+	//Aluguel
+	r.HandleFunc("/carrosAlugados", rest.GetAlugueis).Methods("GET") //Template
+	r.HandleFunc("/alugar", rest.AlugarCarro)                        //Template
+	return r
+}
---
+++ aluguel/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	routes "github.com/LeandroAlcantara-1997/controller/router"
+	utils "github.com/LeandroAlcantara-1997/controller/utils"
+	"github.com/LeandroAlcantara-1997/model/repository"
+)
+
+func main() {
+	r := routes.NewRouter()
+	http.Handle("/", r)
+
+	//Cria as tabelas assim que o programa é executado
+	utils.LoadTemplates("view/*.html")
+	err := repository.CreateTables()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Serivdor rodando porta 8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Erro ao rodar servidor ", err)
+	}
+
+}
